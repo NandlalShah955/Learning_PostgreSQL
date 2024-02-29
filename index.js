8eadc34880c703f5fc17cdcf51a766b105e94fdc
“Nandlal_shah” <shahnandlal49@gmail.com>
2024-02-29 22:52:19 +0530

    basic server setup

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello WOrld");
+import express from "express";
+const port = 8000;
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello from nandlal");
+});
+app.listen(port, () => {
+  console.log(`Server is listening on Port ${port}`);
+});
