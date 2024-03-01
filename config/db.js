65d897593551b52e64d6f82320c45639c57728a1
“Nandlal_shah” <shahnandlal49@gmail.com>
2024-03-01 22:54:32 +0530

    adding database connection with postgresql

---
+++ config/db.js
@@ -0,0 +1,19 @@
+import { Sequelize } from "sequelize";
+export const connection = async () => {
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.HOST,
+      dialect: process.env.DIALECT,
+    }
+  );
+
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connection } from "./config/db.js";
 dotenv.config();
 const port = process.env.port;
 const app = express();
+
+connection();
 app.get("/", (req, res) => {
   res.send("Hello from nandlal");
 });
