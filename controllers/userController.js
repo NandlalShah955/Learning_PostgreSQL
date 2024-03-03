e3c63dae40ae710cf1ae00bfac71b1eb35116b26
“Nandlal_shah” <shahnandlal49@gmail.com>
2024-03-03 20:37:01 +0530

    added controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,12 @@ class UserController {
   static CreateUser = (req, res) => {
     try {
       const { email } = req.body;
-    } catch (error) {res.status(500).json({message:''})}
+    } catch (error) {
+      res.status(500).json({
+        message: "OOPS there is some error while creating the user ",
+        error: error.message,
+      });
+    }
   };
   static GetUser = (req, res) => {};
   static UpdateUser = (req, res) => {};
